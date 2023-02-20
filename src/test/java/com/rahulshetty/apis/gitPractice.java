6ac01cf768f4d84b87915cae9d4e15cb57866a75
Prashanth <eskillaprashanth7@gmail.com>
2023-02-20 22:24:04 +0530

    git practice added

---
+++ src/test/java/com/rahulshetty/apis/gitPractice.java
@@ -0,0 +1,10 @@
+package com.rahulshetty.apis;
+
+import org.testng.annotations.Test;
+
+public class gitPractice {
+    @Test
+    public void gitTest(){
+        System.out.println("git test");
+    }
+}
